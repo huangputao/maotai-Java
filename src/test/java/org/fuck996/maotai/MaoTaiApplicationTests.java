9dbea31acc9d10922c58c39ed60f2e4ba5835f40
huang <183433307@qq.com>
2024-03-27 11:46:49 +0800

    提交

--- src/test/java/org/fuck996/maotai/MaoTaiApplicationTests.java
+++ src/test/java/org/fuck996/maotai/MaoTaiApplicationTests.java
@@ -1,13 +1,17 @@
 package org.fuck996.maotai;
 
+import org.fuck996.maotai.service.MaoTaiService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MaoTaiApplicationTests {
-
+    @Autowired
+    private MaoTaiService maoTaiService;
     @Test
-    void contextLoads() {
+    void contextLoads() throws Exception {
+        maoTaiService.pushMessageForWinningRecord();
     }
 
 }
